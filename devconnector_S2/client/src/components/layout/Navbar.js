01810b804414ce7b54632bd02d6bc1d0ad59c471
Abdessamad EL HAFI <abdessamad_elhafi@um5.ac.ma>
2021-01-24 19:58:05 +0100

    the before booting version

--- devconnector_S2/client/src/components/layout/Navbar.js
+++ devconnector_S2/client/src/components/layout/Navbar.js
@@ -7,17 +7,17 @@ import { connect } from 'react-redux';
 const Navbar = ({auth : {isAuthenticated , loading },logout  }) => {
   const authLinks = (
     <ul>
-        <li><Link to="Dashboard"><i className="fas fa-user"></i>{'  '} <span className="hide-sm">Dashboard</span></Link></li>
-        <li><Link to="profiles">Instructeurs</Link></li>
-        <li><Link to="posts"><i className="fas fa-arow"></i>{'  '} <span className="hide-sm">Posts</span></Link></li>
-        <li><Link to ="login" onClick={logout}> <i className="fas fa-sign-out-alt"></i>{'  '} <span className="hide-sm">Logout</span></Link></li>
+        <li><Link to="Dashboard"><i className="fas fa-user"></i>{'  '} <span className="hide-sm">Tableau de bord</span></Link></li>
+        <li><Link to="profiles">Etudiants</Link></li>
+        <li><Link to="posts"><i className="fas fa-arow"></i>{'  '} <span className="hide-sm">Discussion</span></Link></li>
+        <li><Link to ="login" onClick={logout}> <i className="fas fa-sign-out-alt"></i>{'  '} <span className="hide-sm">Déconnexion</span></Link></li>
 
       </ul>
 
   );
   const guestLinks = (
     <ul>
-        <li><Link to="/profiles">Instructeurs</Link></li>
+        <li><Link to="/profiles">Etudiants</Link></li>
         <li><Link to="/register">Inscription</Link></li>
         <li><Link to="/login">Connexion</Link></li>
       </ul>
