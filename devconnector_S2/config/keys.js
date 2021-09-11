c7bf33607d993b2a9fb88ac0341ae3af66bc8e81
el hafi <abdessamad_elhafi@um5.ac.ma>
2021-09-11 11:17:49 +0100

    delete unused files and folders

--- devconnector_S2/config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  
-  mongoURI: 'mongodb://abdo123:abdo456@cluster0-shard-00-00-iepf8.mongodb.net:27017,cluster0-shard-00-01-iepf8.mongodb.net:27017,cluster0-shard-00-02-iepf8.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
-  // mongoURI: 'mongodb+srv://abdo123:abdo456@cluster0.lgvs4.mongodb.net/draft?retryWrites=true&w=majority',
-  secret : 'jwtScret'
-};
-          
